a0563c1722dd6d32e60e959958f91b366871ed95
st-barts <stbartss@proton.me>
2026-05-16 22:13:08 +0800

    feat: initial setup with example manifest

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/st-barts/stremio-streamed-addon
+
+go 1.26.3
+
+require github.com/syumai/workers v0.33.0
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/syumai/workers"
+)
+
+var manifestJson = []byte(`{
+	"id": "app.stbarts.stremio-streamed-addon",
+	"version": "0.0.1",
+	"name": "Stremio Streamed Live Sports Addon",
+	"description": "A Stremio addon that serves live streams from streamed",
+	"resources": ["catalog", "stream"],
+	"types": ["tv"],
+	"catalogs": [
+		{
+			"type": "tv",
+			"id": "streamed-live-sports",
+		}
+	],
+	"idPrefixes": ["streamed-live-sports"]
+}`)
+
+func main() {
+	http.HandleFunc("/manifest.json", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(manifestJson)
+	})
+
+	workers.Serve(nil) // use http.DefaultServeMux
+}
