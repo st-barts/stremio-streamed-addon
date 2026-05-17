acfb892e5516fc3faaa05434193afd8b140aebb8
st-barts <stbartss@proton.me>
2026-05-17 15:42:36 +0800

    fix: added cors middleware handler

--- handlers/manifest.go
+++ handlers/manifest.go
@@ -1,1 +1,24 @@
 package handlers
+
+import "net/http"
+
+var manifestJson = []byte(`{
+	"id": "app.stbarts.stremio-streamed-addon",
+	"version": "0.0.1",
+	"name": "Stremio Streamed Live Sports Addon",
+	"description": "A Stremio addon that serves live streams from streamed",
+	"resources": ["catalog", "stream"],
+	"types": ["tv"],
+	"catalogs": [
+		{
+			"type": "tv",
+			"id": "streamed-live-sports"
+		}
+	],
+	"idPrefixes": ["streamed-live-sports"]
+}`)
+
+func ManifestHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(manifestJson)
+}
--- main.go
+++ main.go
@@ -3,30 +3,12 @@ package main
 import (
 	"net/http"
 
+	"github.com/st-barts/stremio-streamed-addon/handlers"
 	"github.com/syumai/workers"
 )
 
-var manifestJson = []byte(`{
-	"id": "app.stbarts.stremio-streamed-addon",
-	"version": "0.0.1",
-	"name": "Stremio Streamed Live Sports Addon",
-	"description": "A Stremio addon that serves live streams from streamed",
-	"resources": ["catalog", "stream"],
-	"types": ["tv"],
-	"catalogs": [
-		{
-			"type": "tv",
-			"id": "streamed-live-sports"
-		}
-	],
-	"idPrefixes": ["streamed-live-sports"]
-}`)
-
 func main() {
-	http.HandleFunc("/manifest.json", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(manifestJson)
-	})
+	http.HandleFunc("/manifest.json", handlers.ManifestHandler)
 
-	workers.Serve(nil) // use http.DefaultServeMux
+	workers.Serve(handlers.CorsMiddleware(http.DefaultServeMux)) // use http.DefaultServeMux
 }
