36647940b6935e6d24e6a56c547573042021171d
st-barts <stbartss@proton.me>
2026-09-19 22:54:58 +0800

    fix: remove unavailable streamed endpoint

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,6 @@
 export const STREAMED_ORIGINS = [
   // These are the known origins for streamed's web app and API. We can use any of these to make requests to the API, as they all share the same backend.
   "https://streamed.pk",
-  "https://streami.su",
   "https://streamed.st",
 ];
 
